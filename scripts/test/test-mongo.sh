192e9829989f647f5ae2133336a3dd37c9e88c31
Simon Tse <simon.tse.hk@gmail.com>
2023-10-14 15:49:33 +0800

    mongo-3 cannot be tested on arm64 for now

--- scripts/test/test-mongo.sh
+++ scripts/test/test-mongo.sh
@@ -9,8 +9,9 @@ cd $projRoot
 
 # TESTS
 cd $projRoot/mongodb-5-0-21 && make down up wait-healthy && cd -
-cd $projRoot/mongodb-3-2-16 && make down up wait-healthy && cd -
+# 3-2-16 failed on arm64
+#cd $projRoot/mongodb-3-2-16 && make down up wait-healthy && cd -
 
 # CLEAN UP
 cd $projRoot/mongodb-5-0-21 && make down
-cd $projRoot/mongodb-3-2-16 && make down
+#cd $projRoot/mongodb-3-2-16 && make down
